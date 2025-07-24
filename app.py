0a7c20b949b3dd43562bb2fef244bc876b361989
Iñigo Amonarriz <iamonarriz@CPU00325U.vicomtech.es>
2025-07-24 13:21:23 +0200

    v1

--- app.py
+++ app.py
@@ -3,8 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def euskera():
+    return render_template('index.html', 
+        language='euskera',
+        logo='img/logo-tree.png',
+        headline1='APRENDE EUSKERA',
+        headline2='APRENDE ESPAÑOL',
+        content_title='MINTZAPRAKTIKA',
+        lang_icon='🇪🇺', 
+        lang_name='EUSKERA'
+    )
+
+@app.route('/espanol')
+def espanol():
+    return render_template('index.html', 
+        language='espanol',
+        logo='img/logo-tree-white.png',
+        headline1='APRENDE ESPAÑOL',
+        headline2='APRENDE ESPAÑOL',
+        content_title='ESPAÑOL',
+        lang_icon='🇪🇸',
+        lang_name='ESPAÑOL'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
